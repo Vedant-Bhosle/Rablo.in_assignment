8fbc6a6db74cdf65984b06b4680f19cbe3b4435b
Vedant Bhosle <vedantlb11@gmail.com>
2024-01-28 15:22:30 +0530

    a

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use(
 );
 // app.options("*", cors());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   // another common pattern
   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
